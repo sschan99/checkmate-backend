271d460e790c58604503d93bf0efad711843bc55
Kim Seungchan <42964952+sschan99@users.noreply.github.com>
2024-06-09 23:35:54 +0900

    chore: 프로젝트 초기 세팅

---
+++ src/test/java/backend/checkmate/CheckmateApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.checkmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckmateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
